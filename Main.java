b95ed8db7f8a66ad463b642945ca078dc8e2807c
Nikhil Reddy <nikhilreddy.maligireddy1@gmail.com>
2020-03-05 17:01:43 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		String inputPath = fileToPath("database.txt");
+		String outputPath = "results.txt";
+
+		int minUtil = 40;
+
+		AHUS4 ahus = new AHUS4();
+
+		ahus.runAlgorithm(inputPath, outputPath, minUtil);
+	}
+
+	private static String fileToPath(String filename) throws UnsupportedEncodingException {
+		URL url = Main.class.getResource(filename);
+
+		return java.net.URLDecoder.decode(url.getPath(), "UTF-8");
+	}
+}
